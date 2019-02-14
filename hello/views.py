68a0d0756efadfd95ef9022cf0fb5cfb2de4ffd4
trakbit <harsh.vardhan.611@gmail.com>
2019-02-14 19:38:11 +0530

    linting  error

--- hello/views.py
+++ hello/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 from .models import Greeting
 
 # Create your views here.
+
+
 def index(request):
+
     # return HttpResponse('Hello from Python!')
     return render(request, "index.html")
 
