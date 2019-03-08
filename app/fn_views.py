bcfd4fbfd6704ad627554f0f791783d8361d4bbc
trakbit <harsh.vardhan.611@gmail.com>
2019-03-08 20:43:34 +0530

    get contracts by weeks/months

--- app/fn_views.py
+++ app/fn_views.py
@@ -4,6 +4,9 @@ from upstox_api.api import Session, Upstox
 import json
 import itertools as it
 from app.models import Option
+from datetime import datetime
+import calendar
+from dateutil import relativedelta
 
 api_key = 'Qj30BLDvL96faWwan42mT45gFHyw1mFs8JxBofdx'
 
@@ -34,15 +37,22 @@ def getMasterContract(request):
     accessTokenData = json.loads(accessToken)
     upstox = Upstox(api_key, accessTokenData['accessToken'])
     upstox.get_master_contract('NSE_FO')
-    optionSearch = upstox.search_instruments('NSE_FO', 'reliance19may')  
+    optionSearch = upstox.search_instruments('NSE_FO', 'reliance')
+    #Get First and Last Day of the Current Month/Week
+    today = datetime.now().today() + relativedelta.relativedelta(weeks=1)
+    first_day_date = datetime(today.year, today.month, 1).timestamp() * 1000
+    last_day = calendar.monthrange(today.year, today.month)[1]
+    last_day_date = datetime(today.year, today.month, last_day).timestamp() * 1000
     #Creating Python Objects of all options
     allOptions = []
     for ops in optionSearch:
-        allOptions.append(Option(
-            ops[0], ops[1], ops[2], ops[3], ops[4],
-            ops[5], ops[6], ops[7], ops[8], ops[9],
-            ops[10], ops[11]
-        ))
+        expiry = int(ops[6])
+        if expiry >= first_day_date and expiry <= last_day_date:
+            allOptions.append(Option(
+                ops[0], ops[1], ops[2], ops[3], ops[4],
+                ops[5], ops[6], ops[7], ops[8], ops[9],
+                ops[10], ops[11]
+            ))
     #saperating calls and puts
     optionPairs = []
     for a, b in it.combinations(allOptions, 2):
