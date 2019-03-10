873d542754a80d01128f1cd00dd5df752dfb57c2
trakbit <harsh.vardhan.611@gmail.com>
2019-03-10 18:01:45 +0530

    Enable CORS

--- upstox_server/settings.py
+++ upstox_server/settings.py
@@ -41,10 +41,12 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "app",
     'rest_framework',
-    'rest_framework_swagger'
+    'rest_framework_swagger',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
