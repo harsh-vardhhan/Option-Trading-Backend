d809b985de82e20970c9c404118ed395675af119
trakbit <harsh.vardhan.611@gmail.com>
2019-05-13 09:30:43 +0530

    disable cace on view

--- app/fn_views.py
+++ app/fn_views.py
@@ -207,7 +207,7 @@ def save_full_quotes(request):
         ).save()
     return Response({"Message": "Quotes Saved"})
 
-@api_view(['POST'])
+@api_view(['POST'], no_cache=True)
 def get_full_quotes(request):
     list_options = Full_Quote.objects.all().order_by('strike_price')
     def pairing():
