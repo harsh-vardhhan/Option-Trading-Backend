1d5c234f0da50489284a9f4a875e59ec865240da
trakbit <harsh.vardhan.611@gmail.com>
2019-06-04 13:46:31 +0530

    quote update

--- app/consumers.py
+++ app/consumers.py
@@ -61,6 +61,7 @@ class stock_consumer(AsyncWebsocketConsumer):
       connection.close()   
       u.start_websocket(True)
       def quote_update(message):
+         print(message)
          stock_consumer.send_message(self, message)
       u.set_on_quote_update(quote_update)
 
