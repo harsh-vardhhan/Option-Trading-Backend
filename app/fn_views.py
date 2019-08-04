34b99c85b834730adc0d4c961ea375c510563bc0
trakbit <harsh.vardhan.611@gmail.com>
2019-08-04 22:36:39 +0530

    remove chart class

--- app/fn_views.py
+++ app/fn_views.py
@@ -3,7 +3,7 @@ from rest_framework.views import Response
 from upstox_api.api import Session, Upstox
 import json
 import itertools as it
-from app.models import Instrument, Full_Quote, Expiry_Date, Chart
+from app.models import Instrument, Full_Quote, Expiry_Date
 from datetime import datetime, date
 import calendar
 from dateutil import relativedelta
@@ -126,8 +126,11 @@ def cal_strategy(request):
     max_loss_expiry = 100000000000000000000000
     max_loss_numerical = 0
 
-    def toJson(self):
-        return {key: getattr(self, key, None) for key in self.__slots__}
+    def obj_dict(obj):
+        return obj.__dict__
+
+    def toJson(func):
+        return json.loads(json.dumps(func, default=obj_dict))
 
     for i, symbol in enumerate(symbols):
 
@@ -318,37 +321,37 @@ def cal_strategy(request):
                 # Mini Chart
                 if(spot_symbol_type == "PE"):
                     if j == 1:
-                        mini_chart = Chart(
-                            symbol=spot_symbol_trim,
-                            strike_price=round(spot_price),
-                            profit=r.get("pp_"+spot_symbol_trim)
-                                    .decode("utf-8")
-                        )
+                        mini_chart = {
+                            "symbol": spot_symbol_trim,
+                            "strike_price": round(spot_price),
+                            "profit": r.get("pp_"+spot_symbol_trim)
+                                       .decode("utf-8")
+                        }
                         mini_analysis_chart.append(toJson(mini_chart))
                     for strike_symbol in buy_sell_strike:
                         if (spot_symbol_trim == strike_symbol):
-                            mini_chart = Chart(
-                                symbol=spot_symbol_trim,
-                                strike_price=round(spot_price),
-                                profit=r.get("pp_"+spot_symbol_trim)
-                                        .decode("utf-8")
-                            )
+                            mini_chart = {
+                                "symbol": spot_symbol_trim,
+                                "strike_price": round(spot_price),
+                                "profit": r.get("pp_"+spot_symbol_trim)
+                                           .decode("utf-8")
+                            }
                             mini_analysis_chart.append(toJson(mini_chart))
                     if j == last_instrument:
-                        mini_chart = Chart(
-                            symbol=spot_symbol_trim,
-                            strike_price=round(spot_price),
-                            profit=r.get("pp_"+spot_symbol_trim)
-                                    .decode("utf-8")
-                        )
+                        mini_chart = {
+                            "symbol": spot_symbol_trim,
+                            "strike_price": round(spot_price),
+                            "profit": r.get("pp_"+spot_symbol_trim)
+                                       .decode("utf-8")
+                        }
                         mini_analysis_chart.append(toJson(mini_chart))
 
-                    chart = Chart(
-                        symbol=spot_symbol_trim,
-                        strike_price=round(spot_price),
-                        profit=r.get("pp_"+spot_symbol_trim)
-                                .decode("utf-8")
-                    )
+                    chart = {
+                        "symbol": spot_symbol_trim,
+                        "strike_price": spot_price,
+                        "profit": r.get("pp_"+spot_symbol_trim).decode("utf-8")
+                    }
+
                     analysis_chart.append(toJson(chart))
 
     if (premium_paid >= 0):
